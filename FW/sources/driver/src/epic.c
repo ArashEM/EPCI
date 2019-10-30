c526beb2d7683ac256ca83011e4b03844b56d50e
arash <arash.golgol@gmail.com>
2019-10-30 18:58:29 +0330

    Add draft for file operations methods

--- FW/sources/driver/src/epic.c
+++ FW/sources/driver/src/epic.c
@@ -21,8 +21,42 @@ struct epci_priv {
 };
 
 
+/**
+*	epci file operations 
+*/
+static ssize_t epci_read(struct file * file, char __user * buf, size_t count, loff_t *offset)
+{
+	return 0;
+}
 
+static ssize_t epci_write(struct file * file, const char __user * buf, size_t count, loff_t *offset)
+{
+	return 0;
+}
 
+static loff_t epci_llseek(struct file * file, loff_t offset, int whence)
+{
+	return 0;
+}
+
+static int epci_open(struct inode * inode, struct file * file)
+{
+	return 0;
+}
+
+static int epci_release(struct inode * inode, struct file * file)
+{
+	return 0;
+}
+
+static const struct file_operations epci_fops = {
+	.owner   = THIS_MODULE,
+	.read    = epci_read,
+	.write   = epci_write,
+	.llseek  = epci_llseek,
+	.open    = epci_open,
+	.release = epci_release,
+};
 
 /**
 *	pci id table
@@ -39,6 +73,7 @@ MODULE_DEVICE_TABLE(pci, epci_pci_tbl);
 */
 static int epci_probe(struct pci_dev *dev, const struct pci_device_id *id)
 {
+	printk(KERN_NOTICE "PCI device: vendor: %#04x, device: %#04x \r\n",dev->vendor, dev->device);
 	return 0;
 }
 
