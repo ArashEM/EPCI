7a8d384a02a6016d717f6b8b8a57c6879d8353b7
arash <arash.golgol@gmail.com>
2019-11-28 22:52:37 +0330

    Apply new firmware version (V1.04)

--- FW/sources/hdl/packages/board_config_pkg.vhd
+++ FW/sources/hdl/packages/board_config_pkg.vhd
@@ -18,7 +18,7 @@ use IEEE.STD_LOGIC_1164.all;
 package board_config_pkg is
 
 	constant	EPCI_FW_MAJOR	:	std_logic_vector(7 downto 0)	:= x"01";
-	constant	EPCI_FW_MINOR	:	std_logic_vector(7 downto 0)	:= x"03";
+	constant	EPCI_FW_MINOR	:	std_logic_vector(7 downto 0)	:= x"04";
 	constant	EPCI_BUILD_NUM	:	std_logic_vector(15 downto 0)	:= x"0000";
 	constant	EPCI_FW_VER		:	std_logic_vector(31 downto 0)	
 								:=	EPCI_FW_MAJOR & 
