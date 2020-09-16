a334521df1084e2d680a45e435a8339044bed6fd
arash <arash.golgol@gmail.com>
2020-09-16 16:52:54 +0430

    Add structure for 'epci-mem' test program

---
+++ FW/sources/tests/mem/mem_bt.c
@@ -0,0 +1,10 @@
+/**
+*  EPCI memory black box test programm
+*/
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    printf("EPCI-V1.0x memory blackbox test program.\r\n");
+    return 0;
+}
